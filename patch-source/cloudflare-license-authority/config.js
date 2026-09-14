551e1c1f8e71521c9b8107e2baf48223b40a8f59
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-14 20:38:26 -0300

    feat: point clinical app at same-origin Cloudflare runtime

---
+++ patch-source/cloudflare-license-authority/config.js
@@ -0,0 +1,8 @@
+window.DEBORA_APP_CONFIG = Object.freeze({
+  SUPABASE_URL: window.location.origin,
+  SUPABASE_PUBLISHABLE_KEY: 'cloudflare-runtime',
+  BACKEND_MODE: 'cloudflare',
+  APP_NAME: 'Débora Lactação',
+  ALLOWED_EMAIL: '',
+  DEMO_MODE: false
+});
